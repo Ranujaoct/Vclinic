23d4d474e424cd5f8613087d1a118c2750afce8b
Ranuja <Ranuja@HMG-KH3>
2017-06-26 18:02:50 +0530

    Add project files.

---
+++ vClinic/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(vClinic.Startup))]
+namespace vClinic
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
